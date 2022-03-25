f4a61b593be93dc085e36f8b1fefbba98d2388f2
lisz1012 <lisz1012@163.com>
2022-03-24 23:00:49 -0700

    refactor

--- src/main/java/com/lisz/eurekaregisterserver/EurekaRegisterServerApplication.java
+++ src/main/java/com/lisz/eurekaregisterserver/EurekaRegisterServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer // Eureka服务端必备
 @SpringBootApplication
 public class EurekaRegisterServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaRegisterServerApplication.class, args);
 	}
-
 }
